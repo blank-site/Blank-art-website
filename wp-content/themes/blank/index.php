3b1233226c189035aadac5832a01ded660df2c04
Yi Liu <yi.liu@triggerglobal.com>
2011-09-09 20:49:35 +0800

    added a new theme :blank

---
+++ wp-content/themes/blank/index.php
@@ -0,0 +1,2 @@
+<?php get_header();  ?>
+<?php get_template_part('top','banner')?>
---
+++ wp-content/themes/blank/top-banner.php
@@ -0,0 +1,9 @@
+<div class="location">
+Home > <?php wp_nav_menu()  ?><?php edit_post_link( __( 'Edit', 'twentyeleven' ), '<span class="edit-link">', '</span>' ); ?>
+</div>
+<div class='page-title'>
+
+</div>
+<div class="page-subtitle">
+
+</div>
